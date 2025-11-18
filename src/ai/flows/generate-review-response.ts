ff712ee59b66280a1167ed9fc73a848f94734e38
Christian WONG <wongchristian68@gmail.com>
2025-11-18 14:26:30 +0000

    Try fixing this error: `Console Error: Error: Failed to fetch from https

--- src/ai/flows/generate-review-response.ts
+++ src/ai/flows/generate-review-response.ts
@@ -48,7 +48,7 @@ const generateReviewResponseFlow = ai.defineFlow(
 
     const llmResponse = await ai.generate({
       prompt: prompt,
-      model: 'googleai/gemini-1.5-flash-latest',
+      model: 'googleai/gemini-1.5-pro-latest',
       config: {
         maxOutputTokens: 200,
       },
